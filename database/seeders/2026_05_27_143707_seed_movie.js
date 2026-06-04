99682bf887812ca757e57bde27b3aa2e9aff2332
Szép Andor <andorbandi08@gmail.com>
2026-06-04 15:03:50 +0200

    filmek hozzáadása manuálisan az adatbázishoz

--- database/seeders/2026_05_27_143707_seed_movie.js
+++ database/seeders/2026_05_27_143707_seed_movie.js
@@ -3,7 +3,10 @@ import db from '../../app/models/modrels.js';
 async function up({context: QueryInterface}) {
   if(db.Movie) {
     await db.Movie.bulkCreate([
-      
+      { id: 1, title: 'Eredet', director: 'Christopher Nolan', release: 2010 },
+      { id: 2, title: 'Ponyvaregény', director: 'Quentin Tarantino', release: 1994 },
+      { id: 3, title: 'Gladiátor', director: 'Ridley Scott', release: 2000 },
+      { id: 4, title: 'A keresztapa', director: 'Francis F. Coppola', release: 1972 }
     ]);
   }else {
     await QueryInterface.bulkInsert('movies', [
